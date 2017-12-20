7b3b58211983f4dd8714d49816e1b8ec3238248e
4000D <dkdkajej@gmail.com>
2017-12-20 20:05:48 +0900

    g

--- script/fifsDemo.js
+++ script/fifsDemo.js
@@ -8,24 +8,26 @@ const web3 = new Web3(rpcEndpoint);
 
 // parameters
 const networkId = "777";
+const domain = "4000d.eth";
 let accounts;
 let contractOwner;
 let domainOwner;
-const domain = namehash("dapp-study.eth");
 
 // contracts
 const ENSArtifact = require("../build/contracts/ENS.json");
 const FIFSRegistrarArtifact = require("../build/contracts/FIFSRegistrar.json");
+const PublicResolverArtifact = require("../build/contracts/PublicResolver.json");
 
-const ENS = new web3.eth.Contract(ENSArtifact.abi, ENSArtifact.networks[ networkId ].address);
-const FIFSRegistrar = new web3.eth.Contract(FIFSRegistrarArtifact.abi, FIFSRegistrarArtifact.networks[ networkId ].address);
+const ens = new web3.eth.Contract(ENSArtifact.abi, ENSArtifact.networks[ networkId ].address);
+const fifsRegistrar = new web3.eth.Contract(FIFSRegistrarArtifact.abi, FIFSRegistrarArtifact.networks[ networkId ].address);
+const publicResolver = new web3.eth.Contract(PublicResolverArtifact.abi, PublicResolverArtifact.networks[ networkId ].address);
 
 const main = async () => {
   try {
     await loadAccounts();
-
-    const r1 = await FIFSRegistrar.methods.register(namehash("dapp-study"), domainOwner)
-      .send({ from: contractOwner });
+    logAddrs();
+    await registerDomain(domain);
+    await getAddr(domain);
   } catch (e) {
     console.error(e);
   }
@@ -40,4 +42,70 @@ async function loadAccounts() {
   accounts = await web3.eth.getAccounts();
   contractOwner = accounts[ 0 ];
   domainOwner = accounts[ 1 ];
+}
+
+async function registerDomain(domain) {
+  console.log("\nregisterDomain");
+  console.log(`register "${ domain } to ${ domainOwner }"`);
+
+  const label = domain.split(".")[ 0 ];
+
+  const labelHash = web3.utils.soliditySha3(label);
+  const domainNamehash = namehash(domain);
+
+  const r = await fifsRegistrar.methods.register(labelHash, domainOwner)
+    .send({ from: contractOwner });
+
+  console.log("register tx", r.transactionHash);
+
+  const _domainOwner = await ens.methods.owner(domainNamehash).call();
+
+  console.log("domainOwner", domainOwner);
+  console.log("_domainOwner", _domainOwner);
+
+  const r2 = await ens.methods.setResolver(publicResolver.address);
+
+  const r3 = await publicResolver.methods.setAddr(domainNamehash, publicResolver.address)
+    .send({ from: domainOwner });
+
+  console.log("register public resolver to", publicResolver._address);
+
+  console.log(`
+domain \t ${ domain }
+
+label \t ${ label }
+labelHash \t ${ labelHash }
+domainNamehash \t ${ domainNamehash }`);
+}
+
+async function getAddr(domain) {
+  console.log("\ngetAddr");
+
+  const domainNamehash = namehash(domain);
+
+  const resolverAddr = await ens.methods.resolver(domainNamehash).call();
+
+  console.log(`
+resolverAddr \t ${ resolverAddr }`);
+
+  const resolver = new web3.eth.Contract(PublicResolverArtifact.abi, resolverAddr);
+
+  const addr = await resolver.methods.addr(domainNamehash).call();
+
+  console.log(`
+domain \t ${ domain }
+domainNamehash \t ${ domainNamehash }
+address of ${ domain } \t ${ addr }
+`);
+}
+
+function logAddrs() {
+  console.log(`
+contract owner : ${ contractOwner }
+domain owner : ${ domainOwner }
+
+ENS: ${ ens._address }
+FIFSRegistrar: ${ fifsRegistrar._address }
+PublicResolver: ${ publicResolver._address }
+`);
 }
